0c7b5846e760e3894b377f7499e4ed64a2eff5a3
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-11 23:21:20 +0300

    logo and webrtc handler

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,30 @@
 import * as wss from "./wss.js";
 import * as store from './store.js';
+import * as webRTCHandler from './webRTCHandler.js';
+import * as constants from './constants.js';
 
 // initialization of socketIO connection
 const socket = io();
 wss.registerSocketEvents(socket);
 
-// personal code copy button
 const personalCodeCopyButton = document.getElementById('personal_code_copy_button');
 personalCodeCopyButton.addEventListener('click', () => {
     const personalCode = store.getState().socketId;
     navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
+
+const personalCodeChatButton = document.getElementById('personal_code_chat_button');
+personalCodeChatButton.addEventListener('click', () => {
+    const calleePersonalCode = document.getElementById('personal_code_input').value;
+    const callType = constants.callType.CHAT_PERSONAL_CODE;
+
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+});
+
+const personalCodeVideoButton = document.getElementById('personal_code_video_button');
+personalCodeVideoButton.addEventListener('click', () => {
+    const calleePersonalCode = document.getElementById('personal_code_input').value;
+    const callType = constants.callType.VIDEO_PERSONAL_CODE;
+
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
 });
---
+++ public/js/webRTCHandler.js
@@ -0,0 +1,5 @@
+export const sendPreOffer = (callType, personalCode) => {
+    console.log(callType, personalCode);
+
+    
+}
