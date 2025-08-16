ee30153a9f07bac1c73a3d2b3bce9ae92f11954d
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-16 23:58:37 +0300

    stun servers

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -19,24 +19,24 @@ const defaultConstraints = {
 
 const configuration = {
     iceServers: [
-        // {
-        //     urls: 'stun:stun.l.google.com:19302'
-        // },
-        // {
-        //     urls: 'stun:stun1.l.google.com:19302'
-        // },
-        // {
-        //     urls: 'stun:stun2.l.google.com:19302'
-        // },
-        // {
-        //     urls: 'stun:stun3.l.google.com:19302'
-        // },
-        // {
-        //     urls: 'stun:stun4.l.google.com:19302'
-        // },
+        {
+            urls: 'stun:stun.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun1.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun2.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun3.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun4.l.google.com:19302'
+        },
         {
             urls: [
-                // 'stun:185.233.47.117:3478',
+                'stun:185.233.47.117:3478',
                 'turn:185.233.47.117:3478?transport=udp',
                 'turn:185.233.47.117:3478?transport=tcp'
             ],
