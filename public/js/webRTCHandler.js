2e6228e6e136bb294c50ca6878bdbf8027bc2591
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-16 23:37:50 +0300

    Turn on turn

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -34,15 +34,15 @@ const configuration = {
         // {
         //     urls: 'stun:stun4.l.google.com:19302'
         // },
-        // {
-        //     urls: [
-        //         'stun:185.233.47.117:3478',
-        //         'turn:185.233.47.117:3478?transport=udp',
-        //         'turn:185.233.47.117:3478?transport=tcp'
-        //     ],
-        //     username: 'webrtcuser',
-        //     credential: 'strongpassword'
-        // }
+        {
+            urls: [
+                // 'stun:185.233.47.117:3478',
+                'turn:185.233.47.117:3478?transport=udp',
+                'turn:185.233.47.117:3478?transport=tcp'
+            ],
+            username: 'webrtcuser',
+            credential: 'strongpassword'
+        }
     ],
     iceCandidatePoolSize: 10
 }
